833171a13dd2eab64a1a803693c248809a51c105
abbo-servoo <salyabbo@gmail.com>
2023-10-06 12:31:17 +0100

    add select navbar for translation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  lang: string = '';
+
+  ngOnInit(): void {
+    this.lang =  localStorage.getItem('lang') || 'en';
+  }
+
+  changeLang(lang: any){
+    const selectedLanguage = lang.target.value;
+    localStorage.setItem('lang', selectedLanguage);
+  }
 
 }
